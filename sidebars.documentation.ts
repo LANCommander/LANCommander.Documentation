beb9c9bdab47ed379e723a047b2bd1254887de2a
Pat Hartl <pat@pathar.tl>
2026-03-23 00:56:27 -0500

    Add getting started to sidebar

--- sidebars.documentation.ts
+++ sidebars.documentation.ts
@@ -15,6 +15,7 @@ import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
 const sidebars: SidebarsConfig = {
   documentationSidebar: [
     'Overview',
+    'GettingStarted',
     {
       type: 'category',
       label: 'Releases',
