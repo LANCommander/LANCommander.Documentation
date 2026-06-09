2a0eb90218e0d37ecefdf3c2f9db80f47abcc466
Pat Hartl <pat.h@vanguardinc.com>
2026-06-09 16:30:53 -0500

    Disable user color scheme

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -127,7 +127,7 @@ const config: Config = {
     colorMode: {
       defaultMode: 'dark',
       disableSwitch: true,
-      respectPrefersColorScheme: true,
+      respectPrefersColorScheme: false,
     },
     navbar: {
       logo: {
